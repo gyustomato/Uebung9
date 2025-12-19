c992b7cb4f6168822cf8a969ce430ce227e30004
gyustomato <ddurmaz@smail.uni-koeln.de>
2025-12-20 00:47:01 +0100

    Uebung9.3

--- src/h1/PrioListe.java
+++ src/h1/PrioListe.java
@@ -1,5 +1,28 @@
 package h1;
 
-public class PrioListe {
+import java.util.ArrayList;
 
+public class PrioListe {
+	public ArrayList<Patient> myList = new ArrayList<>();
+
+	public void addPatient(Patient p) {
+		if (myList.isEmpty()) {
+			myList.add(p);
+		} else {
+			for (int i = myList.size(); i >= 0; i--) {
+				if (myList.get(i).prio > p.prio) {
+					myList.add(i, p);
+				}
+			}
+		}
+	}
+	
+	public Patient getNextPatient() {
+		 return myList.removeFirst();
+		 // oder: Patient raus = myList.getFirst();   myList.remove(0);   return raus;
+	}
+	
+	public int getPosition(Patient p) {
+		return myList.indexOf(p);
+	}
 }
